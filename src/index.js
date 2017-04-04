c04af4d3fc0bb7863f2f6639d803df861b7d3dfa
A-A-Z <dramatichamster@gmail.com>
2017-04-05 01:47:42 +1000

    Added materialize-loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import 'materialize-css/sass/materialize.scss'
+// import 'materialize-css/sass/materialize.scss'
 import './assets/scss/app.scss'
 
 ReactDOM.render(
